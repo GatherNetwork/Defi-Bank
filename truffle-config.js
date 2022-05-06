854a30389332a89b766fc9aacc163b787536d4bf
shaquib21 <nehal.shaquib21@gmail.com>
2022-05-06 23:02:16 +0530

    basic functionality done

---
+++ truffle-config.js
@@ -0,0 +1,79 @@
+require('babel-register');
+require('babel-polyfill');
+const dotenv = require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+// const HDWalletProvider = require('@truffle/hdwallet-provider');  // @notice - Should use new module.
+const mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 gwei
+      network_id: 42
+    },
+    main: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://main.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 gwei
+      network_id: 1
+    },
+    gather: {
+      provider: () => new HDWalletProvider(mnemonic, `https://testnet.gather.network`),
+      port: 80,
+      network_id: "*",
+      // gas: 4700000,
+      // skipDryRun: true
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 gwei
+      network_id: 4
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 gwei
+      network_id: 3
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      version: ">=0.6.0 <0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
